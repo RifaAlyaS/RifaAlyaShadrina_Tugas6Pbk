044e7a740ad50affcb12ea5729952c29a5412520
RifaAlyaS <rifaalyashadrina@student.uir.ac.id>
2024-05-30 23:18:11 +0700

    first commit

--- src/components/Software.vue
+++ src/components/Software.vue
@@ -1,40 +1,47 @@
 <template>
   <div class="q-pa-md row items-start q-gutter-md">
-    <q-card class="my-card">
-      <q-img src="https://i.pinimg.com/474x/d5/ed/ca/d5edcac92858f3b608ae5ba6e68a780d.jpg">
+    <q-card v-for="(card, index) in cards" :key="index" class="my-card">
+      <q-img :src="card.img">
         <div class="absolute-bottom">
-          <div class="text-h6">Fl Studio</div>
-          <div class="Price">200.000</div>
+          <div class="text-h6">{{ card.title }}</div>
+          <div class="Price">{{ card.price }}</div>
         </div>
       </q-img>
 
       <q-card-actions>
         <q-btn outline color="black" label="Wish List" />
         <q-btn flat color="black" label="Detail" />
-      </q-card-actions>                 
+        <q-btn flat color="red" label="Remove" @click="removeCard(index)" />
+      </q-card-actions>
     </q-card>
-
-    <q-card class="my-card">
-      <q-img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtl83uHcdp2-KhMaxHYw03tb8aEpPYmkTkTjwJ-QV6cGlW4g0NLTJDgdLvmp7ASvcgr_o&usqp=CAU">
-        <div class="absolute-bottom">
-          <div class="text-h6">Adobe After Effects</div>
-          <div class="Price">Rp 8.000.000/tahun</div>
-        </div>
-      </q-img>
-
-      <q-card-actions>
-        <q-btn outline color="black" label="Wish List" />
-        <q-btn flat color="black" label="Detail" />
-      </q-card-actions>                 
-    </q-card>
-
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Software'
-}
+  name: 'Software',
+  data() {
+    return {
+      cards: [
+        {
+          img: 'https://i.pinimg.com/474x/d5/ed/ca/d5edcac92858f3b608ae5ba6e68a780d.jpg',
+          title: 'Fl Studio',
+          price: 'Rp 200.000'
+        },
+        {
+          img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtl83uHcdp2-KhMaxHYw03tb8aEpPYmkTkTjwJ-QV6cGlW4g0NLTJDgdLvmp7ASvcgr_o&usqp=CAU',
+          title: 'Adobe After Effects',
+          price: 'Rp 8.000.000/tahun'
+        }
+      ]
+    };
+  },
+  methods: {
+    removeCard(index) {
+      this.cards.splice(index, 1);
+    }
+  }
+};
 </script>
 
 <style scoped>
